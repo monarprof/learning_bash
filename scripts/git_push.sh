8f7c60166f1f9404edd8edb1d9b948df704f9322
Nara B <monarprof@gmail.com>
2023-11-08 23:51:38 +0000

    updated

--- scripts/git_push.sh
+++ scripts/git_push.sh
@@ -3,12 +3,12 @@
 if [ -d " .git" ] ; then
    git add .
    if [ $# -le 0 ] ;then
-   read -p "please enter commit message:" commit_message
+   read -p "please enter commit message: " commit_message
 
    git commit -m "$commit_message"
 else 
 
-   git commit -m $1
+   git commit -m "$1"
 fi
    git push
 else 
