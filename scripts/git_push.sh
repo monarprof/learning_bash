e4404a51eb3bfd56a8bb7c8e843310d56793c7bc
Nara B <monarprof@gmail.com>
2023-11-08 02:21:06 +0000

    updated

--- scripts/git_push.sh
+++ scripts/git_push.sh
@@ -1,11 +1,18 @@
-#!/bin/bash
-git add .
-if [ $# -le 0 ] ;then
-read -p "please enter commit message" commit_message
 
-git commit -m "$commit_message"
+#!/bin/bash
+if [ -d " .git" ] ; then
+   git add .
+   if [ $# -le 0 ] ;then
+   read -p "please enter commit message" commit_message
+
+   git commit -m "$commit_message"
 else 
 
-git commit -m $1
+   git commit -m $1
 fi
-git push
+   git push
+else 
+
+echo "You are not in repository "
+fi
+
